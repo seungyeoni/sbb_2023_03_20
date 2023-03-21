de72f20e3dcc61e5d7c5ffa0e373108713f30eef
seungyeoni <dnri33@naver.com>
2023-03-21 15:20:47 +0900

    더 객체지향적

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -25,7 +25,18 @@ public class Question {
 
     private LocalDateTime createDate; // DATETIME
 
+    // OneToMany 자바세상에서의 편의를 위해서 필드 생성
+    // 이 녀석은 실제 DB 테이블에 칼럼이 생성되지 않는다.
+    // DB는 배열이나 리스트를 저장할 수 없다.
+    // 칼럼에 저장할 수 없다.
+    // 만들어도 되고 안 만들어도 됩니다.
+    // 다만 만들면 해당 객체(질문객체)에서 관련된 답변들을 찾을 때 편합니다.
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    // OneToMany에는 직접 객체초기화
+    // OneToMany 에는 직접객체초기화
     private List<Answer> answerList = new ArrayList<>();
+
+    public void addAnswer(Answer a) {
+        a.setQuestion(this);
+        answerList.add(a);
+    }
 }
