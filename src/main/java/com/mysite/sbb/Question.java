6dcfb86e88ab24795a64964a65f249aa40147c53
seungyeoni <dnri33@naver.com>
2023-03-21 13:50:05 +0900

    양방향 관계 설정

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 // question 테이블이 생김
 @Getter
@@ -22,4 +24,8 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate; // DATETIME
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    // OneToMany에는 직접 객체초기화
+    private List<Answer> answerList = new ArrayList<>();
 }
