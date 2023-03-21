17e628aa8af6a7466fb72dc7f943be255af424a9
seungyeoni <dnri33@naver.com>
2023-03-21 10:54:07 +0900

    데이터 삭제

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -112,4 +112,15 @@ class SbbApplicationTests {
 		q.setSubject("수정된 제목");
 		this.questionRepository.save(q);
 	}
+
+	@Test
+	@DisplayName("데이터 삭제")
+	void t008() {
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
 }
