4c738c6ec26a85e3dac9c2cde910d976a24dac5e
seungyeoni <dnri33@naver.com>
2023-03-23 16:21:13 +0900

    오류 메시지 공통 템플릿

--- src/main/java/com/mysite/sbb/answer/AnswerForm.java
+++ src/main/java/com/mysite/sbb/answer/AnswerForm.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.answer;
 
 import jakarta.validation.constraints.NotEmpty;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,6 @@ import lombok.Setter;
 @Setter
 public class AnswerForm {
     @NotEmpty(message = "내용은 필수항목입니다.")
+    @Size(max = 200, message = "내용을 200자 이하로 입력해주세요.")
     private String content;
 }
--- src/main/java/com/mysite/sbb/question/QuestionForm.java
+++ src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -10,10 +10,10 @@ import lombok.Setter;
 @Setter
 public class QuestionForm {
     @NotEmpty(message="제목은 필수항목입니다.")
-    @Size(max=200)
+    @Size(max = 200, message = "제목을 200자 이하로 입력해주세요.")
     private String subject;
 
     @NotEmpty(message="내용은 필수항목입니다.")
-    @Size(max=20000)
+    @Size(max = 200, message = "내용을 20,000자 이하로 입력해주세요.")
     private String content;
 }
